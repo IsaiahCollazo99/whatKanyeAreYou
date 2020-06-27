16dacd43732e2abe1c115142fcc90c6faac5f783
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2020-06-27 10:32:15 -0400

    install react-router-dom and create LandingPage component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
+import LandingPage from './components/LandingPage';
 
 function App() {
   return (
     <div className="App">
-      App
+      <Switch>
+        <Route exact path={"/"}>
+          <LandingPage />
+        </Route>
+      </Switch>
     </div>
   );
 }
---
+++ frontend/src/components/LandingPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import quizzes from '../util/quizzes';
+
+const LandingPage = () => {
+    const quizzesList = quizzes.map(quiz => {
+        
+    })
+    
+    return (
+        <div className="landingContainer">
+            Landing
+        </div>
+    )
+}
+
+export default LandingPage;
