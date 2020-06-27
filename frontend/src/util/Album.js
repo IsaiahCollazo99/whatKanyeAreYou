250f241b339c3855c5876c352756a0fd385f330f
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2020-06-27 07:16:05 -0400

    create Album class

---
+++ frontend/src/util/Album.js
@@ -0,0 +1,8 @@
+class Album {
+    constructor( name, cover, youtubeLink, spotifyLink ) {
+        this.name = name;
+        this.cover = cover;
+        this.youtubeLink = youtubeLink;
+        this.spotifyLink = spotifyLink;
+    }
+}
